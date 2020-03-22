94c0e904449f99b17c5777090795999736042830
Renato <rntdek@gmaail.com>
2020-03-22 20:34:21 -0300

    js - add, calcularhora, total, classe tr - active

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,7 +3,36 @@
 const sr = ScrollReveal();
 sr.reveal('.card');
 
+// Calcula valor total
+const calcFeature = (devHours, testHours) => {
+  let valorHora = document.getElementById("valorHora").value;
+  let valorFeature = Math.floor((devHours + testHours) * valorHora);
+  return valorFeature;
+}
 
+// Atribui classe para tr selecionado
+const selecionar = event => {
+  let trs = document.getElementsByTagName("tr");
+  for(let tr of trs){
+    tr.className = "";
+  }
+
+  event.target.parentNode.classList.add("table-active");
+}
+
+// Gera partes da tabela
+const geraTabela = (id, data) => {
+  return `
+    <tr onclick="selecionar(event)" data-index="${id}">
+      <td>${data.feature}</td>
+      <td>${data.devHours}h</td>
+      <td>${data.testHours}h</td>
+      <td>R$ ${calcFeature(data.devHours, data.testHours)}</td>
+    </tr>
+  `;
+}
+
+let totalAmount = 0;
 let features = [
   {
     feature: "Authentication",
@@ -13,6 +42,59 @@ let features = [
 ];
 
 
+// Atualiza
+const att = () => {
+  totalAmount = 0;
+  let table = document.getElementById("bodyTable");
+  table.innerHTML = "";
+  features.forEach((feature, i) => {
+    let tr = geraTabela(i, feature);
+    table.insertAdjacentHTML("beforeend", tr);
+  });
+
+  document.getElementById("totalDev").innerHTML = features.reduce(
+    (acc, feature) => (acc += feature.devHours),
+    0
+  );
+  document.getElementById("totalTest").innerHTML = features.reduce(
+    (acc, feature) => (acc += feature.testHours),
+    0
+  );
+  document.getElementById("total").innerHTML = features.reduce(
+    (acc, feature) =>
+      (acc += calcFeature(feature.devHours, feature.testHours)),
+    0
+  );
+};
+
+const init = () => att();
+init();
+
+document.getElementById("valorHora").addEventListener("keyup", () => {
+  att();
+});
+
+// Adiciona valores do form na tabela
+document.getElementById("btnAdd").addEventListener("click", e => {
+  e.preventDefault();
+
+  let inputFeature = document.getElementById("addName");
+  let inputDev = document.getElementById("addDev");
+  let inputTest = document.getElementById("addTest");
+  let feature = {
+    feature: inputFeature.value,
+    devHours: parseInt(inputDev.value),
+    testHours: parseInt(inputTest.value)
+  };
+  document.getElementById("addForm").reset();
+
+  features.push(feature);
+  att();
+});
+
+
+
+// Exibição da div de form e solucao footer
 function addDiv(){
   div = document.getElementById("addDiv");
   footer = document.getElementById("footer")
